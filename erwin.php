31ab6d1ef730aa39f60cbc6817eb82ae044bfa44
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-11 02:07:26 +0800

    Create a widget - with popular post

--- erwin.php
+++ erwin.php
@@ -80,3 +80,84 @@ function my_display_views($column_name) {
 }
 
 add_action( 'manage_posts_custom_column', 'my_display_views', 5, 2);
+
+
+
+
+/**
+ * Adds Popular Posts widget.
+ */
+class popular_posts extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+				'popular_posts', // Base ID
+				__( 'Popular Posts', 'text_domain' ), // Name
+				array( 'description' => __( 'Display the 5 most popular posts', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		echo __( 'Hello, World!', 'text_domain' );
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Popular posts', 'text_domain' );
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class Foo_Widget
+
+// register popular_posts widget
+function register_popular_posts_widget() {
+	register_widget( 'popular_posts' );
+}
+add_action( 'widgets_init', 'register_popular_posts_widget' );
+
+
+
