358d7f951f185f02e6519d13d3b720928daaa5ab
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-11 00:56:46 +0800

    Added wordpress plugin information

---
+++ erwin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Erwin
+ * @version 1.0
+ */
+/*
+Plugin Name: Erwin
+Plugin URI: http://fashionsponge.com
+Description: Rock in roll to the world
+Author: Jesus Erwin Suarez
+Version: 1.0
+Author URI: http://erwin.com
+*/
